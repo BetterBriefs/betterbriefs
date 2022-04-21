eb9fb8341d0020e68e1747e02287413b92d71d4c
Kerstin Reichinger <kr@cognify.ai>
2022-04-21 14:26:55 +0200

    setup react app with firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase/app'
+import { getFirestore } from '@firebase/firestore'
+import { getStorage } from 'firebase/storage'
+import { getAuth } from 'firebase/auth'
+
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+})
+
+export const db = getFirestore(app)
+export const auth = getAuth(app)
+export const storage = getStorage(app)
