55853292b2d5be0062d91ae90cb22dcf2c29b336
Kerstin Reichinger <kr@cognify.ai>
2022-04-25 14:13:21 +0200

    update project, test with dummy data

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { storage, db } from "../../firebase-config";
-import { collection, getDocs, getDoc, doc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
 import { useParams } from "react-router-dom";
 import { ref, getDownloadURL } from "firebase/storage";
+import { Button } from "../button/Button";
 
 export const Profile = () => {
   // userId from URL
@@ -50,6 +57,24 @@ export const Profile = () => {
     setProjects(parsedData);
   }
 
+  function publishProject(project) {
+    const docRef = doc(db, "projects", project.id);
+    updateProject(docRef);
+    getProjects();
+  }
+
+  // Update project / dummy data for test purpose
+  // Todo: create form which user submits to update project
+  async function updateProject(projectDocRef) {
+    await updateDoc(projectDocRef, {
+      description: "A description for my done project",
+      repo_link: "the link for my repo link",
+      thumbnail: "the preview image",
+      title: "A new title",
+      published: true,
+    });
+  }
+
   return (
     <>
       {userProfile && projects && (
@@ -62,6 +87,7 @@ export const Profile = () => {
               <div>State: {project.published.toString()}</div>
               {/* TODO imageurl is undefined at this point, but after method it is correct */}
               <img src={getImageUrl(project.thumbnail)} alt="project"></img>
+              <Button onClick={() => publishProject(project)}>Publish</Button>
             </>
           ))}
         </>
