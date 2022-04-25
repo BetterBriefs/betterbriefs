ac725daf82cf98e82270f2fe00ba3679c01f8d3f
Kerstin Reichinger <kr@cognify.ai>
2022-04-25 14:29:21 +0200

    logout in header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,12 +2,26 @@ import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import logoUrl from "../../logo.svg";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase-config";
+import { Button } from "../button/Button";
+
+async function logoutUser() {
+  signOut(auth)
+    .then(() => {
+      console.log("logout");
+    })
+    .catch(() => {
+      alert("logout went wrong");
+    });
+}
 
 export const Header = ({ user }) => (
   <div className="header">
     <img src={logoUrl} alt="Tesla" />
     <div>
-      <Link to="/sign-in">Login</Link>
+      {user && <Button onClick={logoutUser}>Logout</Button>}
+      {!user && <Link to="/sign-in">Login</Link>}
       <Link to="/">MyList</Link>
       {user && <Link to={`/profile/${user.uid}`}>Profile</Link>}
     </div>
