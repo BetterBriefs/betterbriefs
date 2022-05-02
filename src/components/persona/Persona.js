39c1e3ceb84e3303daf7fce538e735c2882205f9
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-05-02 18:35:07 +0200

    added spans around values to help when testing

--- src/components/persona/Persona.js
+++ src/components/persona/Persona.js
@@ -16,7 +16,7 @@ export const Persona = ({ personaUrl, persona }) => {
         <div className="persona_description">
           <h3>{persona.name}</h3>
           <div className="persona_tags">
-            <h4>Age</h4> {persona.age} | <h4>Sex</h4> {personaSex}
+            <h4>Age</h4> <span>{persona.age}</span> | <h4>Sex</h4> <span>{personaSex}</span>
           </div>
           <h4>About</h4>
           <p>{persona.about}</p>
