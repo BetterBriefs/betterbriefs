8a896e27b3ff88124d4a13665d51a9af44be201d
Tanja Santner <tsantner.mmt-b2020@fh-salzburg.ac.at>
2022-05-02 15:34:41 +0200

    Delete useEffect from color palette

--- src/components/color_palette/ColorPalette.js
+++ src/components/color_palette/ColorPalette.js
@@ -1,25 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { ColorElement } from "../color_palette/ColorElement";
 import { Card } from "../card/Card";
 import "./ColorPalette.css";
 
 export const ColorPalette = ({ colors }) => {
-
-    useEffect(() => {
-        console.log(colors)
-    }, [colors])
-
   return (
     <Card>
       <h2>Colors</h2>
       <div className="flexbox">
-      <ColorElement color={colors.color1}></ColorElement>
-      <ColorElement color={colors.color2}></ColorElement>
-      <ColorElement color={colors.color3}></ColorElement>
-      <ColorElement color={colors.color4}></ColorElement>
-      <ColorElement color={colors.color5}></ColorElement>
+        <ColorElement color={colors.color1}></ColorElement>
+        <ColorElement color={colors.color2}></ColorElement>
+        <ColorElement color={colors.color3}></ColorElement>
+        <ColorElement color={colors.color4}></ColorElement>
+        <ColorElement color={colors.color5}></ColorElement>
       </div>
     </Card>
   );
 };
-
