83151a70412c669b29d17e6a020ee85346aee27c
Tanja Santner <tsantner.mmt-b2020@fh-salzburg.ac.at>
2022-05-02 14:54:35 +0200

    Update component color palette

--- src/components/color_palette/ColorPalette.js
+++ src/components/color_palette/ColorPalette.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { ColorElement } from "../color_palette/ColorElement";
 import { Card } from "../card/Card";
+import "./ColorPalette.css";
 
 export const ColorPalette = ({ colors }) => {
   return (
     <Card>
       <h2>Colors</h2>
-      <div>
+      <div className="flexbox">
         {colors.map((color) => {
           return <ColorElement color={color}></ColorElement>;
         })}
