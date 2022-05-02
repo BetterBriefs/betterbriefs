fdb3f60e60bf113eee2d3d0c919a7af57d0952ca
Kerstin Reichinger <kr@cognify.ai>
2022-05-02 15:43:29 +0200

    format whole code

--- src/components/brief/Brief.js
+++ src/components/brief/Brief.js
@@ -34,7 +34,7 @@ export const Brief = () => {
     font: undefined,
     layout: undefined,
     idea: undefined,
-    persona: undefined,
+    persona: undefined
   });
 
   // database
@@ -46,31 +46,31 @@ export const Brief = () => {
 
   async function getColors() {
     const data = await getDocs(colorsCollectionRef);
-    const parsedData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    const parsedData = data.docs.map(doc => ({ ...doc.data(), id: doc.id }));
     setColors(parsedData);
   }
 
   async function getFonts() {
     const data = await getDocs(fontsCollectionRef);
-    const parsedData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    const parsedData = data.docs.map(doc => ({ ...doc.data(), id: doc.id }));
     setFonts(parsedData);
   }
 
   async function getIdeas() {
     const data = await getDocs(ideasCollectionRef);
-    const parsedData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    const parsedData = data.docs.map(doc => ({ ...doc.data(), id: doc.id }));
     setIdeas(parsedData);
   }
 
   async function getLayouts() {
     const data = await getDocs(layoutsCollectionRef);
-    const parsedData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    const parsedData = data.docs.map(doc => ({ ...doc.data(), id: doc.id }));
     setLayouts(parsedData);
   }
 
   async function getPersonas() {
     const data = await getDocs(personasCollectionRef);
-    const parsedData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    const parsedData = data.docs.map(doc => ({ ...doc.data(), id: doc.id }));
     setPersonas(parsedData);
   }
 
@@ -93,13 +93,11 @@ export const Brief = () => {
       let layoutid = getIdOfParam("l");
       let personaid = getIdOfParam("p");
       setBrief({
-        color: colors.find((color) => color.id === colorid),
-        font: fonts.find((font) => font.id === fontid.toString()),
-        persona: personas.find(
-          (persona) => persona.id === personaid.toString()
-        ),
-        idea: ideas.find((idea) => idea.id === ideaid.toString()),
-        layout: layouts.find((layout) => layout.id === layoutid.toString()),
+        color: colors.find(color => color.id === colorid),
+        font: fonts.find(font => font.id === fontid.toString()),
+        persona: personas.find(persona => persona.id === personaid.toString()),
+        idea: ideas.find(idea => idea.id === ideaid.toString()),
+        layout: layouts.find(layout => layout.id === layoutid.toString())
       });
     } else {
       setBrief({
@@ -107,7 +105,7 @@ export const Brief = () => {
         font: undefined,
         layout: undefined,
         idea: undefined,
-        persona: undefined,
+        persona: undefined
       });
       setBriefGenerated(false);
     }
@@ -141,20 +139,20 @@ export const Brief = () => {
     let randomIdeaIndex = Math.floor(Math.random() * lengthIdeas) + 1;
 
     // type of idea and layout must match
-    let idea = ideas.find((idea) => idea.id === randomIdeaIndex.toString());
+    let idea = ideas.find(idea => idea.id === randomIdeaIndex.toString());
 
-    let filteredLayouts = layouts.filter((layout) => layout.type === idea.type);
+    let filteredLayouts = layouts.filter(layout => layout.type === idea.type);
     lengthLayouts = filteredLayouts.length;
     let randomLayoutIndex = Math.floor(Math.random() * lengthLayouts) + 1;
 
     setBrief({
-      color: colors.find((color) => color.id === randomColorIndex.toString()),
-      font: fonts.find((font) => font.id === randomFontIndex.toString()),
+      color: colors.find(color => color.id === randomColorIndex.toString()),
+      font: fonts.find(font => font.id === randomFontIndex.toString()),
       persona: personas.find(
-        (persona) => persona.id === randomPersonaIndex.toString()
+        persona => persona.id === randomPersonaIndex.toString()
       ),
-      idea: ideas.find((idea) => idea.id === randomIdeaIndex.toString()),
-      layout: layouts[randomLayoutIndex],
+      idea: ideas.find(idea => idea.id === randomIdeaIndex.toString()),
+      layout: layouts[randomLayoutIndex]
     });
 
     navigate(
@@ -184,7 +182,7 @@ export const Brief = () => {
     if (brief.layout) {
       const path = ref(storage, brief.layout.link);
 
-      getDownloadURL(path).then((url) => {
+      getDownloadURL(path).then(url => {
         // Insert url into an <img> tag
         setLayoutUrl(url);
       });
@@ -196,7 +194,7 @@ export const Brief = () => {
     if (brief.persona) {
       const path = ref(storage, brief.persona.avatar);
 
-      getDownloadURL(path).then((url) => {
+      getDownloadURL(path).then(url => {
         // Insert url into an <img> tag
         setPersonaUrl(url);
       });
@@ -225,7 +223,7 @@ export const Brief = () => {
       {briefGenerated === true && (
         <>
           <Idea idea={brief.idea}></Idea>
-          
+
           <Persona personaUrl={personaUrl} persona={brief.persona}></Persona>
           <br></br>
 
