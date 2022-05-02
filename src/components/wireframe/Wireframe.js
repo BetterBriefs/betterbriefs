86486d015ec280accb9f0bc40cf23656d3dcddcb
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-05-02 14:48:41 +0200

    removed unneeded styling

--- src/components/wireframe/Wireframe.js
+++ src/components/wireframe/Wireframe.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Card } from "../card/Card";
-import './Wireframe.css';
 
 export const Wireframe = ({ layoutUrl}) => {
 
     return (
         <Card>
             <h2>Layout</h2>
-            <img src={layoutUrl} class="wireframe__img" alt="layout"></img>
+            <img src={layoutUrl} alt="layout"></img>
         </Card>
     );
 };
