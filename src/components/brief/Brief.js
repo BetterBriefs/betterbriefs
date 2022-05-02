9819a444e5507878fb95e5284b994e0e159529da
Tanja Santner <tsantner.mmt-b2020@fh-salzburg.ac.at>
2022-05-02 15:54:01 +0200

    Update styling color palette

--- src/components/brief/Brief.js
+++ src/components/brief/Brief.js
@@ -5,7 +5,6 @@ import { storage, db } from "../../firebase-config";
 import { collection, getDocs } from "firebase/firestore";
 import { Button } from "../button/Button";
 import { Wireframe } from "../wireframe/Wireframe";
-import { Persona } from "../persona/Persona";
 import { Idea } from "../idea/Idea";
 import { ColorPalette } from "../color_palette/ColorPalette";
 import { Fonts } from "../fonts/Fonts";
@@ -34,7 +33,7 @@ export const Brief = () => {
     font: undefined,
     layout: undefined,
     idea: undefined,
-    persona: undefined
+    persona: undefined,
   });
 
   // database
@@ -46,31 +45,31 @@ export const Brief = () => {
 
   async function getColors() {
     const data = await getDocs(colorsCollectionRef);
-    const parsedData = data.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+    const parsedData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
     setColors(parsedData);
   }
 
   async function getFonts() {
     const data = await getDocs(fontsCollectionRef);
-    const parsedData = data.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+    const parsedData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
     setFonts(parsedData);
   }
 
   async function getIdeas() {
     const data = await getDocs(ideasCollectionRef);
-    const parsedData = data.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+    const parsedData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
     setIdeas(parsedData);
   }
 
   async function getLayouts() {
     const data = await getDocs(layoutsCollectionRef);
-    const parsedData = data.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+    const parsedData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
     setLayouts(parsedData);
   }
 
   async function getPersonas() {
     const data = await getDocs(personasCollectionRef);
-    const parsedData = data.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+    const parsedData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
     setPersonas(parsedData);
   }
 
@@ -93,11 +92,13 @@ export const Brief = () => {
       let layoutid = getIdOfParam("l");
       let personaid = getIdOfParam("p");
       setBrief({
-        color: colors.find(color => color.id === colorid),
-        font: fonts.find(font => font.id === fontid.toString()),
-        persona: personas.find(persona => persona.id === personaid.toString()),
-        idea: ideas.find(idea => idea.id === ideaid.toString()),
-        layout: layouts.find(layout => layout.id === layoutid.toString())
+        color: colors.find((color) => color.id === colorid),
+        font: fonts.find((font) => font.id === fontid.toString()),
+        persona: personas.find(
+          (persona) => persona.id === personaid.toString()
+        ),
+        idea: ideas.find((idea) => idea.id === ideaid.toString()),
+        layout: layouts.find((layout) => layout.id === layoutid.toString()),
       });
     } else {
       setBrief({
@@ -105,7 +106,7 @@ export const Brief = () => {
         font: undefined,
         layout: undefined,
         idea: undefined,
-        persona: undefined
+        persona: undefined,
       });
       setBriefGenerated(false);
     }
@@ -139,20 +140,20 @@ export const Brief = () => {
     let randomIdeaIndex = Math.floor(Math.random() * lengthIdeas) + 1;
 
     // type of idea and layout must match
-    let idea = ideas.find(idea => idea.id === randomIdeaIndex.toString());
+    let idea = ideas.find((idea) => idea.id === randomIdeaIndex.toString());
 
-    let filteredLayouts = layouts.filter(layout => layout.type === idea.type);
+    let filteredLayouts = layouts.filter((layout) => layout.type === idea.type);
     lengthLayouts = filteredLayouts.length;
     let randomLayoutIndex = Math.floor(Math.random() * lengthLayouts) + 1;
 
     setBrief({
-      color: colors.find(color => color.id === randomColorIndex.toString()),
-      font: fonts.find(font => font.id === randomFontIndex.toString()),
+      color: colors.find((color) => color.id === randomColorIndex.toString()),
+      font: fonts.find((font) => font.id === randomFontIndex.toString()),
       persona: personas.find(
-        persona => persona.id === randomPersonaIndex.toString()
+        (persona) => persona.id === randomPersonaIndex.toString()
       ),
-      idea: ideas.find(idea => idea.id === randomIdeaIndex.toString()),
-      layout: layouts[randomLayoutIndex]
+      idea: ideas.find((idea) => idea.id === randomIdeaIndex.toString()),
+      layout: layouts[randomLayoutIndex],
     });
 
     navigate(
@@ -182,7 +183,7 @@ export const Brief = () => {
     if (brief.layout) {
       const path = ref(storage, brief.layout.link);
 
-      getDownloadURL(path).then(url => {
+      getDownloadURL(path).then((url) => {
         // Insert url into an <img> tag
         setLayoutUrl(url);
       });
@@ -194,7 +195,7 @@ export const Brief = () => {
     if (brief.persona) {
       const path = ref(storage, brief.persona.avatar);
 
-      getDownloadURL(path).then(url => {
+      getDownloadURL(path).then((url) => {
         // Insert url into an <img> tag
         setPersonaUrl(url);
       });
@@ -223,17 +224,19 @@ export const Brief = () => {
       {briefGenerated === true && (
         <>
           <Idea idea={brief.idea}></Idea>
+          <div>
+            <b>Persona:</b>
+          </div>
+          <div>Name: {brief.persona.name}</div>
+          <div>Age: {brief.persona.age}</div>
+          <div>About: {brief.persona.age}</div>
+          <div>Sex: {brief.persona.sex}</div>
+          <img src={personaUrl} alt="persona" width="50"></img>
 
-          <Persona personaUrl={personaUrl} persona={brief.persona}></Persona>
           <br></br>
 
           <br></br>
           <ColorPalette colors={brief.color}></ColorPalette>
-          <b>Colors: </b>
-          <div>
-            {brief.color.color1} {brief.color.color2} {brief.color.color3}{" "}
-            {brief.color.color4} {brief.color.color5}
-          </div>
 
           <br></br>
           <Fonts fonts={brief.font}></Fonts>
