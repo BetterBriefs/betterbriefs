9cc663e938fb92df4e26040c22ada645d8712977
Tanja Santner <tsantner.mmt-b2020@fh-salzburg.ac.at>
2022-05-03 10:41:29 +0200

    Resolve merge conflict

--- src/components/persona/Persona.js
+++ src/components/persona/Persona.js
@@ -12,15 +12,9 @@ export const Persona = ({ personaUrl, persona }) => {
   return (
     <Card>
       <h2>Persona</h2>
-<<<<<<< HEAD
       <div class="persona">
       <LazyImage className="persona_avatar" src={personaUrl} alt="Persona" fallback="fallback_circle shimmer" />
         <div class="persona_description">
-=======
-      <div className="persona">
-        <img className="persona_avatar" src={personaUrl} alt="layout"></img>
-        <div className="persona_description">
->>>>>>> 8287bf40a773ec7701510ebf52bfab5b5b0cf49e
           <h3>{persona.name}</h3>
           <div className="persona_tags">
             <h4>Age</h4> <span>{persona.age}</span> | <h4>Sex</h4>{" "}
