ba06fa344a172142363bd5cb54150ebf858b1c69
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-05-03 12:19:03 +0200

    basic footer styling implemented

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./Footer.css";
+import fhsLogoUrl from "../../fhs-logo-icon.svg";
+
 export const Footer = () => (
-  <div className="footer">
-    <div>About</div>
-    <div>Impressum</div>
-  </div>
+  <section className="footer">
+    <div className="footer__items-container">
+      <ul>
+        <li>Imprint</li>
+        <li>Data Privacy</li>
+      </ul>
+      <ul>
+        <li><a href="https://www.fh-salzburg.ac.at/"><img className="Fhs__logo" src={fhsLogoUrl+""} alt="fh salzburg logo"></img></a></li>
+      </ul>
+    </div>
+  </section>
 );
