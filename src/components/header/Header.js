fae4c5f41582433415d935ca3dc4a9f0ae0727e7
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2022-05-03 12:44:55 +0200

    using better html tag

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 import logoUrl from "../../logo.svg";
 
 export const Header = () => (
-  <div className="header">
+  <section className="header">
     <ul>
       <li><Link to="/"><img src={logoUrl} alt="logo" /></Link></li>
       <li><Link to="/">How-to</Link></li>
       <li><Link to="/">Imprint</Link></li>
     </ul>
-  </div>
+  </section>
 );
