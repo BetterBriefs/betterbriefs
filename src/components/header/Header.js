00de6a30b99e9282077cf04083c99266093ad023
Kerstin Reichinger <kr@cognify.ai>
2022-05-04 08:55:07 +0200

    add favorites to header nav

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,12 +2,25 @@ import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import logoUrl from "../../logo.svg";
+import StarOutlineRoundedIcon from "@mui/icons-material/StarOutlineRounded";
 
 export const Header = () => (
   <section className="header">
     <ul>
-      <li><Link to="/"><img src={logoUrl} alt="logo" /></Link></li>
-      <li><Link to="/about">About</Link></li>
+      <li>
+        <Link to="/">
+          <img src={logoUrl} alt="logo" />
+        </Link>
+      </li>
+      <li>
+        <Link className="favorites" id="favorites" to="/favorites">
+          <StarOutlineRoundedIcon fontSize="large" />
+          Favorites
+        </Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
     </ul>
   </section>
 );
