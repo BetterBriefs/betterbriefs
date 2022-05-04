392d83c3431590148da938b21354823a030ef4a7
Kerstin Reichinger <kr@cognify.ai>
2022-05-04 10:55:02 +0200

    style favorites entry with grid

--- src/components/favorites/FavoritesEntry.js
+++ src/components/favorites/FavoritesEntry.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 import { Card } from "../card/Card";
 import { Tags } from "../tags/Tags";
 import { ColorCircle } from "../color_circle/ColorCircle";
@@ -6,27 +7,41 @@ import { Button } from "../button/Button";
 import "./FavoritesEntry.css";
 
 export const FavoritesEntry = ({ entry, onRemove }) => {
+  const navigate = useNavigate();
+
+  const navToBrief = useCallback(() => navigate(`/${entry.seed}`), [
+    entry,
+    navigate,
+  ]);
+
   return (
     <Card>
-      <div key={entry.seed}>
-        <div class="favorites__card-headline">
-          <h2>{entry.title}</h2>
+      <div className="favorite__container" key={entry.seed}>
+        <h2 className="title">{entry.title}</h2>
+        <div className="actions">
+          <Button usage="primary" onClick={navToBrief}>
+            Read More
+          </Button>
           <Button usage="secondary" onClick={() => onRemove(entry.seed)}>
             Remove
           </Button>
         </div>
+        <div className="tags">
+          <Tags difficulty={entry.difficulty} type={entry.type}></Tags>
+        </div>
 
-        <Tags difficulty={entry.difficulty} type={entry.type}></Tags>
-        <div className="color-line">
+        <div className="colors color-line">
           <ColorCircle color={entry.color1} showCode={false} />
           <ColorCircle color={entry.color2} showCode={false} />
           <ColorCircle color={entry.color3} showCode={false} />
           <ColorCircle color={entry.color4} showCode={false} />
           <ColorCircle color={entry.color5} showCode={false} />
         </div>
-        <h3>Description</h3>
-        <p>{entry.description}</p>
-        <p className="seed">ID: {entry.seed}</p>
+        <div className="description">
+          <h3>Description</h3>
+          <p class="favorites__short-description">{entry.description}</p>
+          <p className="seed">ID: {entry.seed}</p>
+        </div>
       </div>
     </Card>
   );
