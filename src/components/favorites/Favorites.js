295e41d1ada1fd2f7b283bbe91ec6a9d85a92227
Kerstin Reichinger <kr@cognify.ai>
2022-05-04 08:55:57 +0200

    show default text if list is empty

--- src/components/favorites/Favorites.js
+++ src/components/favorites/Favorites.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Card } from "../card/Card";
 import { Tags } from "../tags/Tags";
 import { ColorCircle } from "../color_circle/ColorCircle";
+import "./Favorites.css";
 
 export const Favorites = () => {
   const [favorites, setFavorites] = useState([]);
@@ -10,28 +11,32 @@ export const Favorites = () => {
     setFavorites(currentFavorites);
   }, []);
   return (
-    <>
+    <div className="main-container">
       <h1 className="headline-text">
         <span>Favorites List</span>
       </h1>
-
-      {favorites.map((brief) => (
-        <Card>
-          <div key={brief.seed}>
-            <h2>{brief.title}</h2>
-            <Tags difficulty={brief.difficulty} type={brief.type}></Tags>
-            <div className="color-line">
-              <ColorCircle color={brief.color1} showCode={false} />
-              <ColorCircle color={brief.color2} showCode={false} />
-              <ColorCircle color={brief.color3} showCode={false} />
-              <ColorCircle color={brief.color4} showCode={false} />
-              <ColorCircle color={brief.color5} showCode={false} />
+      {favorites.length > 0 ? (
+        favorites.map((brief) => (
+          <Card>
+            <div key={brief.seed}>
+              <h2>{brief.title}</h2>
+              <Tags difficulty={brief.difficulty} type={brief.type}></Tags>
+              <div className="color-line">
+                <ColorCircle color={brief.color1} showCode={false} />
+                <ColorCircle color={brief.color2} showCode={false} />
+                <ColorCircle color={brief.color3} showCode={false} />
+                <ColorCircle color={brief.color4} showCode={false} />
+                <ColorCircle color={brief.color5} showCode={false} />
+              </div>
+              <h3>Description</h3>
+              <p>{brief.description}</p>
+              <p className="seed">ID: {brief.seed}</p>
             </div>
-            <h3>Description</h3>
-            <p>{brief.description}</p>
-          </div>
-        </Card>
-      ))}
-    </>
+          </Card>
+        ))
+      ) : (
+        <p className="empty-list-text">No entries here yet.</p>
+      )}
+    </div>
   );
 };
