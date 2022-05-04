cff4a30d6167c663594467ea1cd1b7bbb0d37919
Tanja Santner <tsantner.mmt-b2020@fh-salzburg.ac.at>
2022-05-04 19:17:14 +0200

    Add route to fix test case color palette

--- src/components/color_palette/ColorPalette.spec.js
+++ src/components/color_palette/ColorPalette.spec.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ColorPalette } from "./ColorPalette";
 import { cleanup } from "@testing-library/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //initial setup before each test
 let container;
@@ -25,7 +26,18 @@ afterEach(() => {
 describe("Brief ColorPalette", () => {
   it("When a Brief ColorPalette is Generated, a ColorPalette with 5 color elements should be renderd", () => {
     //Act
-    ReactDOM.render(<ColorPalette colors={color}></ColorPalette>, container);
+    ReactDOM.render(
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/favorites"
+            element={<ColorPalette colors={color}></ColorPalette>}
+          />
+        </Routes>
+        <ColorPalette colors={color}></ColorPalette>
+      </BrowserRouter>,
+      container
+    );
     expect(container.querySelectorAll(".element-box")).toHaveProperty(
       "length",
       5
