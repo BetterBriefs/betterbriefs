63a876055a4ea2baa237f73085b0ae103d6bde6b
Kerstin Reichinger <kr@cognify.ai>
2022-05-05 14:01:41 +0200

    memoize function in printbrief

--- src/components/print_brief/Print_brief.js
+++ src/components/print_brief/Print_brief.js
@@ -1,18 +1,15 @@
-import React from "react";
-import PrintIcon from '@mui/icons-material/Print';
+import React, { useCallback } from "react";
+import PrintIcon from "@mui/icons-material/Print";
 import { SidenavElement } from "../sidenav_element/SidenavElement";
 
 export const PrintBrief = () => {
-
-  function printBrief() {
+  const printBrief = useCallback(() => {
     window.print();
-  }
+  }, []);
 
   return (
-    <>
-      <SidenavElement hoverText="Print as PDF" handleClickOpen={printBrief}>
-        <PrintIcon fontSize="large" sx={{ color: "#1f7a83" }} />
-      </SidenavElement>
-    </>
+    <SidenavElement hoverText="Print as PDF" handleClickOpen={printBrief}>
+      <PrintIcon fontSize="large" sx={{ color: "#1f7a83" }} />
+    </SidenavElement>
   );
 };
