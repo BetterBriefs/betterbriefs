c27343c30172fb3db050b031da2dc307bede7023
Kerstin Reichinger <kr@cognify.ai>
2022-05-05 13:58:31 +0200

    remove unneccessary fragment

--- src/components/color_palette/ColorElement.js
+++ src/components/color_palette/ColorElement.js
@@ -3,16 +3,11 @@ import "./ColorElement.css";
 
 export const ColorElement = ({ color }) => {
   return (
-    <>
-      <div className="element-box">
-        <div className="flexbox-center">
-          <div
-            className="circle"
-            style={{ backgroundColor: "#" + color }}
-          ></div>
-        </div>
-        <p>#{color}</p>
+    <div className="element-box">
+      <div className="flexbox-center">
+        <div className="circle" style={{ backgroundColor: "#" + color }}></div>
       </div>
-    </>
+      <p>#{color}</p>
+    </div>
   );
 };
