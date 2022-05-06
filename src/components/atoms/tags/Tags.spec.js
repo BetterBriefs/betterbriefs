a8a6ddfc489f7379a2a933de17325487aceb955b
Tanja Santner <tsantner.mmt-b2020@fh-salzburg.ac.at>
2022-05-06 10:25:21 +0200

    Add test cases for component tags

---
+++ src/components/atoms/tags/Tags.spec.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Tags } from "./Tags";
+import { cleanup, queryByText } from "@testing-library/react";
+
+//initial setup before each test
+let container;
+let difficulty = "normal";
+let type = "shop";
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+  cleanup();
+});
+
+describe("Color Circle", () => {
+    it("When component tags is implemented, two tag elements are rendered.", () => {
+        //Act
+        ReactDOM.render(<Tags difficulty={difficulty} type={type} />, container);
+        expect(container.querySelectorAll(".MuiChip-root")).toHaveProperty("length", 2);
+      });
+    it("When component tags is rendered, text of difficulty is displayed.", () => {
+        //Act
+        ReactDOM.render(<Tags difficulty={difficulty} type={type} />, container);
+        expect(queryByText(container, "normal")).toBeTruthy();
+      });
+    it("When component tags is rendered, text of shop is displayed.", () => {
+        //Act
+        ReactDOM.render(<Tags difficulty={difficulty} type={type} />, container);
+        expect(queryByText(container, "shop")).toBeTruthy();
+    });
+});
